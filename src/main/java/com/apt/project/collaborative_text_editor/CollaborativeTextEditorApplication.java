2594ce1ba1a642794af3647adaea26f3302f11d9
Mohamed Magdy <ymmagdyfofo@gmail.com>
2025-03-30 16:35:38 +0200

    Set up project environment

---
+++ src/main/java/com/apt/project/collaborative_text_editor/CollaborativeTextEditorApplication.java
@@ -0,0 +1,13 @@
+package com.apt.project.collaborative_text_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeTextEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeTextEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apt/project/collaborative_text_editor/CollaborativeTextEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apt.project.collaborative_text_editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeTextEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
